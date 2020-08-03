50829477597b77be9dc91fc8c9d7b715a89413cf
Hebert Flores <2017110474@untels.edu.pe>
2020-08-03 12:33:50 -0500

    agregar modelo y tabla

--- src/java/Modelo/Servicios.java
+++ src/java/Modelo/Servicios.java
@@ -10,5 +10,81 @@ package Modelo;
  * @author USER
  */
 public class Servicios {
-    
+    private int idservicio;
+    private String nombre;
+    private double costo;
+    private String personal;
+    private int tiempo;
+
+    /**
+     * @return the idservicio
+     */
+    public int getIdservicio() {
+        return idservicio;
+    }
+
+    /**
+     * @param idservicio the idservicio to set
+     */
+    public void setIdservicio(int idservicio) {
+        this.idservicio = idservicio;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the costo
+     */
+    public double getCosto() {
+        return costo;
+    }
+
+    /**
+     * @param costo the costo to set
+     */
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    /**
+     * @return the personal
+     */
+    public String getPersonal() {
+        return personal;
+    }
+
+    /**
+     * @param personal the personal to set
+     */
+    public void setPersonal(String personal) {
+        this.personal = personal;
+    }
+
+    /**
+     * @return the tiempo
+     */
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    /**
+     * @param tiempo the tiempo to set
+     */
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
 }
+
+
